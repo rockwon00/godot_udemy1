f40e69af67e4fe767db4db8da4c5ab8ab255c4b0
rockwon00 <kojomo80@gmail.com>
2023-11-12 13:33:24 +0900

    적파괴 로직, 프로젝트 세팅 세부(렌더러,미사용 매개변수 오류무시)

--- scences/basic_enemy/basic_enemy.gd
+++ scences/basic_enemy/basic_enemy.gd
@@ -4,9 +4,9 @@ const MAX_SPEED = 75
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
+	$Area2D.area_entered.connect(on_area_entered)
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = get_direction_to_player()
@@ -18,3 +18,6 @@ func get_direction_to_player():
 	if player_node != null:
 		return (player_node.global_position - global_position).normalized()
 	return Vector2.ZERO #플레이어가 NULL 인 경우
+
+func on_area_entered(other_area:Area2D):
+	queue_free()
