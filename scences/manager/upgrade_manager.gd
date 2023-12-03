24888137a4c68d0857ebc2b212a817ede0d3c234
jm koo <rockwon@outlook.kr>
2023-12-04 00:04:40 +0900

    20231204_업그레이드 옵션_복습필수

--- scences/manager/upgrade_manager.gd
+++ scences/manager/upgrade_manager.gd
@@ -9,16 +9,7 @@ var current_upgrades = {}
 func _ready():
 	experience_manager.level_up.connect(on_level_up)
 	
-func on_level_up(current_level: int):
-	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-	if chosen_upgrade == null:
-		return
-		
-	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
-	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
-	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
-		
+	
 func apply_upgrade(upgrade : AbilityUpgrade):
 	var has_upgrade = current_upgrades.has(upgrade.id)
 	if !has_upgrade: #업그레이드가 없다면
@@ -33,5 +24,27 @@ func apply_upgrade(upgrade : AbilityUpgrade):
 
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
 	
+	
+func pick_upgrades():
+	var chosen_upgrades : Array[AbilityUpgrade] = []
+	# 동일한 요소가 있는 새 배열을 반환
+	var filtered_upgrades = upgrade_pool.duplicate()
+	for i in 2:
+		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
+		chosen_upgrades.append(chosen_upgrade)
+		filtered_upgrades =  filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
+	
+	return chosen_upgrades
+	
+	
 func on_upgrade_selected(upgrade : AbilityUpgrade):
 	apply_upgrade(upgrade)
+
+
+# 모든 신호를 처리하는 메서드를 스크립트 하단에 두는 것을 추천
+func on_level_up(current_level: int):
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+	var chosen_upgrades = pick_upgrades()
+	upgrade_screen_instance.set_ability_upgrades(chosen_upgrades as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
